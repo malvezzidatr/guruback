6d5a8327df85b6f139f17eb4cb8e8b2f88ac4ab8
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2023-03-16 14:47:52 -0300

    Adding firestore

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,4 +1,20 @@
 import UserSchema from "../schemas/UserSchema";
+import { initializeApp } from 'firebase/app'
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvdaijR7fO7son_4KhgCJXWjcMXGiK1xg",
+    authDomain: "guru-0312.firebaseapp.com",
+    projectId: "guru-0312",
+    storageBucket: "guru-0312.appspot.com",
+    messagingSenderId: "241084399475",
+    appId: "1:241084399475:web:e9afbcf609f6cabc5b4f06",
+    measurementId: "G-MHJ9EYQ8EN"
+};
+
+const appFirebase = initializeApp(firebaseConfig)
+export const db = getFirestore(appFirebase)
+
 
 export class UserRepository {
 
@@ -9,6 +25,10 @@ export class UserRepository {
     }
 
     async createUser(name: string, email: string) {
+        return await addDoc(collection(db, 'users'), {
+            name,
+            email
+        })
         return await UserSchema.create({ name, email });
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { UsersRouter } from './routes/UserRouter.routes';
+import cors from 'cors';
 
 const app = express();
-
 app.use(express.json())
+app.use(cors());
+
 app.listen('3030', () => {
     console.log('conectado')
 })
 
 app.use('/users', UsersRouter)
+
